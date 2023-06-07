232dffd514ed8b718862272572a366c216001f29
Zhuo FENG <zhuo@skand.io>
2023-06-07 20:17:25 +0800

    add comments

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,8 @@ import { gql, useMutation } from '@apollo/client';
 import { useEffect } from 'react';
 
 export const Root = () => {
+  // to log in, get a json web token
+  // this account has been seeded to the database in advance
   const [getToken, { data }] = useMutation(gql`
     mutation Token {
       token(email: "test@skand.io", password: "testtest")
@@ -12,5 +14,6 @@ export const Root = () => {
     getToken();
   }, [getToken]);
 
+  // simply show the token for now
   return <>{data?.token}</>;
 };
