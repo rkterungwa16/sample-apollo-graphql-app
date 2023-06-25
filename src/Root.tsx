3a88dcbe0ed0ec63127f7eb49ffe080dce676a4b
rkterungwa16 <terungwakombol@gmail.com>
2023-06-26 00:09:56 +0100

    remove redundant code

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,3 @@
-import { useMutation } from '@apollo/client';
-import { useEffect } from 'react';
-import { LOGIN_USER } from './libraries/graphql/login';
 import { AuthenticationForm } from './libraries/components/auth/Authentication';
 import { Layout } from './libraries/components/layout/Layout';
 
@@ -8,21 +5,9 @@ export const Root = () => {
   // email: "test@skand.io", password: "testtest"
   // to log in, get a json web token
   // this account has been seeded to the database in advance
-  const [getToken, { data }] = useMutation(LOGIN_USER, {
-    variables: {
-      email: 'test@skand.io',
-      password: 'testtest'
-    }
-  });
 
-  useEffect(() => {
-    getToken();
-  }, [getToken]);
-
-  // simply show the token for now
   return (
     <Layout>
-      {data?.token}
       <AuthenticationForm />
     </Layout>
   );
